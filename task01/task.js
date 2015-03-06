a9ff9fe37b1eabadb488c6ae350aaa3493e467b1
Victor Schröder <schrodervictor@gmail.com>
2015-03-06 14:43:04 +0100

    Improves task01 with more hoisting scenarios, object reference and numeric types

--- task01/task.js
+++ task01/task.js
@@ -6,10 +6,60 @@ function test1() {
     console.log(a);
     console.log(foo());
 
-    var a = 1;
+    var a = 10;
     function foo() {
-        return 2;
+        return 10;
     }
 }
 
-test1();
+// Uncomment tho line bellow to check the answer
+//test1();
+
+
+/**
+ * And this one?
+ */
+
+function test2() {
+
+    var a = 020;
+
+    console.log(a);
+    console.log(foo());
+
+    var foo = function() {
+        return 20;
+    };
+}
+
+// Uncomment tho line bellow to check the answer
+//test2();
+
+
+/**
+ * One more...
+ */
+
+
+function test3() {
+
+    var a = {value: 30};
+
+    var b = a;
+
+    b.value = 40;
+
+    console.log(a.value);
+    console.log(b.value);
+
+    console.log(bar());
+
+    var foo = function bar() {
+        return 30;
+    };
+
+    console.log(bar());
+}
+
+// Uncomment tho line bellow to check the answer
+//test3();
