249a3e0528796c0cb4ca54f87a5163ef451c982d
Victor Schröder <schrodervictor@gmail.com>
2015-03-06 14:59:26 +0100

    Adds task08 with basic Git notions

---
+++ task08/task.js
@@ -0,0 +1,35 @@
+/**
+ * Initiate a Git repository in this folder.
+ * Commit each function in a separate commit.
+ * The number denotes the order each function was implemented.
+ *
+ * Create a branch from master to change the functionality of test1.
+ * Create another branch from master to change test1 in a different
+ * way (in other words, create a conflict)
+ *
+ * Merge both branches to master, solving the conflict.
+ */
+
+function test1() {
+
+    return 'test 1';
+
+}
+
+function test4() {
+
+    return 'test 4';
+
+}
+
+function test2() {
+
+    return 'test 2';
+
+}
+
+function test3() {
+
+    return 'test 3';
+
+}
