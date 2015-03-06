2414d9e97a0618b1f981761f7c1d413b9849560b
Victor Schröder <schrodervictor@gmail.com>
2015-03-06 14:54:56 +0100

    Adds task06 with simple jQuery and DOM navigation

---
+++ task06/task.js
@@ -0,0 +1,10 @@
+/**
+ * Select the first item from the first list
+ * the second from the second, and so on.
+ *
+ * You are not allowed to add any new class or id to the html structure.
+ *
+ * Change the color or apply any effect to show your selection.
+ *
+ * You may use jQuery or plain JavaScript in this task.
+ */
