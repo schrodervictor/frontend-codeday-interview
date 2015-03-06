f11a87db7c2c4c75d1fd78dc1069e151366d9cee
Victor Schröder <schrodervictor@gmail.com>
2015-03-06 14:44:33 +0100

    Adds task02 with prototyping notions

---
+++ task02/task.js
@@ -0,0 +1,20 @@
+/**
+ * Define a repeatify function on the String object.
+ * The function accepts an integer that specifies how many times the string has to be repeated.
+ * The function returns the string repeated the number of times specified. For example:
+ *
+ *     console.log('hello'.repeatify(3));
+ */
+
+
+
+
+
+
+
+
+if('hello'.repeatify(3) === 'hellohellohello') {
+    console.log('Success');
+} else {
+    console.log('Failed');
+}
