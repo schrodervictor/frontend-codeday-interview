ba2b52379429360297c8e89c22290aec7267a938
Victor Schröder <schrodervictor@gmail.com>
2015-03-06 14:51:46 +0100

    Adds task05 with implementation overloading and more TDD

---
+++ task05/task.js
@@ -0,0 +1,8 @@
+/**
+ * Write a sum method which will work properly when invoked using either syntax below.
+ */
+
+module.exports.sum = function() {
+
+
+}
---
+++ task05/test.js
@@ -0,0 +1,26 @@
+var expect = require('chai').expect;
+var sum = require('./task').sum;
+
+describe('task.js', function() {
+
+    describe('sum()', function() {
+
+        it('should accept the simple implementation sum(a,b)', function(done) {
+
+            expect(sum(2,5)).to.equals(7);
+
+            done();
+
+        });
+
+        it('should accept the simple implementation sum(a)(b)', function(done) {
+
+            expect(sum(2)(5)).to.equals(7);
+
+            done();
+
+        });
+
+    });
+
+});
